524d558144fb13b2cb118ae38c0b34149506c1fd
Brennan Fee <brennan.git@mailbots.org>
2024-11-28 22:27:08 -0600

    feat: Stub deb-bootstrap configs

---
+++ deb-bootstrap-configs/home-lab.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Author: Brennan A. Fee
+# License: MIT License
+#
+# This script is intended to be used with the [Linux-Bootstraps](https://github.com/brennanfee/linux-bootstraps) deb-bootstrapper.bash script.  See the documentation there for usage.
+#
